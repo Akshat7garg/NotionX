77a7389ea41baed77be3594a888f629d4d710669
Akshat7garg <akshat7garg@gmail.com>
2025-07-09 14:18:40 +0530

    fix: minor cors bug

--- server/app.js
+++ server/app.js
@@ -1,14 +1,12 @@
 import express from "express"
-// import cors from "cors"
+import cors from "cors"
 
 const app = express();
 
-// app.use(cors({
-//     origin: process.env.CORS_ORIGIN,
-//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//     credentials: true,
-//     optionsSuccessStatus: 200
-// }));
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || 'https://localhost:5173',
+    credentials: true,
+}));
 
 app.use(express.json({ limit: '2mb' }));
 app.use(express.urlencoded({ extended: true, limit: '2mb' }));
